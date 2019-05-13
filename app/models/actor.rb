cce9c2a1fb6bcfa2a91a783449b83957d17ef977
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-05-13 06:43:16 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end 
   
   def list_roles 
-    self.characters.collect {|c| "#{c.name} - #{character.show.name}"}
+    self.characters.collect {|c| "#{c.name} - #{c.show.name}"}
   end 
   
 end
