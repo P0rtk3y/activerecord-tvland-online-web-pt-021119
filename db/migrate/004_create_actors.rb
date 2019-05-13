d080940778ac4ef2d20e1472b9606c36f67774ce
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-05-13 06:37:23 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -3,8 +3,7 @@ class CreateActors < ActiveRecord::Migration[4.2]
     create_table :actors do |a|
       a.string :first_name 
       a.string :last_name 
-      a.integer :show_id 
-      a.integer :character_id 
+      a.integer :actor_id
     end 
   end 
 end 
